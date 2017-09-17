88e9d9c1a0afbbf1878f52bef30d9d8115a5e788
Shinta Koyanagi <yancya@upec.jp>
2017-09-17 11:10:19 +0900

    Add test case of HauteCoutureControllerTest#

--- test/controllers/haute_couture_controller_test.rb
+++ test/controllers/haute_couture_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class HauteCoutureControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @headers = { "Authorization" => "Token #{@user.key}"}
+  end
+
+  test "should get query result" do
+    query = <<~SQL
+      SELECT * FROM users
+    SQL
+    get haute_couture_url, params: { haute_couture: { query: query } }, headers: @headers
+
+    assert_response 200
+  end
 end
